ba0b878abcae89c206c85424104f94ae60529b93
Kdiviya <eraiswaryabe@gmail.com>
2025-03-11 16:34:02 -0500

    Add creatingArray.js

---
+++ creatingArray.js
@@ -0,0 +1,11 @@
+let groceryItems = ["Milk", "Egg", "Sugar", "Coffee", "Bread"];
+let itemPrices  = [6.00, 12.00, 5.00];
+let emptyArr = [];
+
+console.log("Grocery Items List: ", groceryItems);
+console.log("Item Prices List: ", itemPrices);
+console.log("Item 1." ,groceryItems[0]);
+console.log("Item 2." ,groceryItems[1]);
+console.log("Item 3." ,groceryItems[2]);
+console.log("Item 1 Price: $",itemPrices[0]);
+console.log("Item 2 Price: $",itemPrices[1]);
